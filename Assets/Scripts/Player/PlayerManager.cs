5f9c0d41f3f19a46eafb3cfc951321df42f587bd
kingkaue <kaueking@gmail.com>
2025-03-25 20:19:30 -0400

    Basic spirit gauge functionality
    Spirit gauge works and changes color when spirit increases and decreases
    
    Press O to increase and P to decrease
    (Used simply to test and will be changed later)

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -17,11 +17,23 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Implememt spirit managing controls here
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            SetSpirit(-10f);
+        }
+
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            SetSpirit(10f);
+        }
     }
 
     public void SetSpirit(float spiritChange)
     {
         spirit += spiritChange;
+        spirit = Mathf.Clamp(spirit, 0, maxSpirit);
+
+        spiritBar.SetSpirit(spirit);
     }
 }
--- Assets/Scripts/UI/SpiritUIManager.cs
+++ Assets/Scripts/UI/SpiritUIManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Numerics;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -5,22 +6,30 @@ using UnityEngine.UI;
 
 public class SpiritUIManager : MonoBehaviour
 {
+    [Header("Bar Properties")]
     [SerializeField] private float spirit;
     [SerializeField] private float maxSpirit;
     [SerializeField] private float width;
     [SerializeField] private float height;
     [SerializeField] private RectTransform spiritBar;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Smoothness")]
+    [SerializeField] private float timeToDrain = 0.25f;
+    private float targetWidth;
+    private float currentWidth;
+    private Coroutine drainSpiritCoroutine;
+
+    [Header("Color Change")]
+    [SerializeField] private Image image;
+    [SerializeField] private Gradient spiritGradient;
+    private Color newSpiritColor;
+
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        image.color = spiritGradient.Evaluate(spirit / maxSpirit);
+        currentWidth = (spirit / maxSpirit) * width;
+        spiritBar.sizeDelta = new UnityEngine.Vector2(currentWidth, height);
     }
 
     public void SetMaxSpirit(float MaxSpirit)
@@ -31,8 +40,40 @@ public class SpiritUIManager : MonoBehaviour
     public void SetSpirit(float Spirit)
     {
         spirit = Spirit;
-        float newWidth = (spirit / maxSpirit) * width;
+        targetWidth = (spirit / maxSpirit) * width;
 
-        spiritBar.sizeDelta = new UnityEngine.Vector2(newWidth, height);
+        // Smooth transition
+        drainSpiritCoroutine = StartCoroutine(DrainSpirit());
+        CheckSpiritGradientAmount();
+    }
+
+    // Coroutine to smoothly show spirit gain and loss
+    private IEnumerator DrainSpirit()
+    {
+        float startWidth = currentWidth;
+        Color currentColor = image.color;
+
+        float elapsedTime = 0f;
+        while (elapsedTime < timeToDrain)
+        {
+            elapsedTime += Time.deltaTime;
+
+            // Lerp length of spirit bar
+            spiritBar.sizeDelta = new UnityEngine.Vector2(Mathf.Lerp(startWidth, targetWidth, (elapsedTime / timeToDrain)), height);
+
+            // Lerp color of spirit bar
+            image.color = Color.Lerp(currentColor, newSpiritColor, (elapsedTime / timeToDrain));
+            yield return null;
+        }
+
+        currentWidth = targetWidth;
+        spiritBar.sizeDelta = new UnityEngine.Vector2(currentWidth, height);
+        drainSpiritCoroutine = null;
+    }
+
+    private void CheckSpiritGradientAmount()
+    {
+        // Gets the new spirit color
+        newSpiritColor = spiritGradient.Evaluate(spirit / maxSpirit);
     }
 }
