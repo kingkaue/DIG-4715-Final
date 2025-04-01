a563c3bb892c8fb56b87aa859d8ba53135a69837
kingkaue <kaueking@gmail.com>
2025-04-01 19:14:13 -0400

    Added changing of colors
    Press E next to journal to change from grayscale to color

---
+++ Assets/Scripts/Change Material.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ChangeMaterial : MonoBehaviour
+{
+    [Header ("Material")]
+    private Renderer objectRenderer;
+    private Material objectMaterial;
+    [SerializeField] Texture2D objectTexture;
+
+    private Coroutine changeMaterialCoroutine;
+    private bool canChange = true;
+    private Vector3 playerPosition;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        objectRenderer = GetComponent<Renderer>();
+        objectMaterial = objectRenderer.material;
+        objectMaterial.SetTexture("_MainTex", objectTexture);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        float distanceFromPlayer = Vector3.Distance(playerPosition, transform.position);
+        if (Input.GetKeyDown(KeyCode.E) && distanceFromPlayer <= 5 && canChange == true)
+        {
+            Debug.Log("Changing Color");
+            canChange = false;
+            changeMaterialCoroutine = StartCoroutine(GrayToColor());
+        }
+    }
+
+    private IEnumerator GrayToColor()
+    {
+        float elapsedTime = 0f;
+        float timeToChange = 2f;
+
+        while (elapsedTime < timeToChange)
+        {
+            elapsedTime += Time.deltaTime;
+            objectMaterial.SetFloat("_saturation", Mathf.Lerp(0, 1, elapsedTime / timeToChange));
+            yield return null;
+        }
+        Debug.Log("Done Changing");
+        changeMaterialCoroutine = null;
+    }
+
+}
