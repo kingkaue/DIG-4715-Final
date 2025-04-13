114033fd6aed1d374d3a13dbbbdef80030d6875d
kingkaue <kaueking@gmail.com>
2025-04-12 20:52:14 -0400

    Added Basked to cabin

--- Assets/Scripts/Game Management/switchtoscene.cs
+++ Assets/Scripts/Game Management/switchtoscene.cs
@@ -1,8 +1,10 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class switchtoscene : MonoBehaviour
 {
+    [SerializeField] private string scene;
     void OnTriggerEnter(Collider other)
     {
         SceneManager.LoadScene("Forest Trail");
