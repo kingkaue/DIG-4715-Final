a0cfeb3ef296db68cd904498249453f081a2cb61
mounirziad <mo697861@ucf.edu>
2025-04-14 12:13:48 -0400

    settingupscenetransition

---
+++ Assets/tutorialcutscene.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class tutorialcutscene : MonoBehaviour
+{
+
+    private ObjectGrabable objectgrabable;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        objectgrabable = GetComponent<ObjectGrabable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (objectgrabable.isGrabbed == true && Input.GetKeyDown("t"))
+        {
+            Debug.Log("Transition Cutscene Started");
+        }
+    }
+}
