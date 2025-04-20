2a6d60ecba78af2ebce511d029aa10f5cd354417
mounirziad <mo697861@ucf.edu>
2025-04-20 12:24:55 -0400

    mhm

--- Assets/gravestoneactivate.cs
+++ Assets/gravestoneactivate.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class gravestoneactivate : MonoBehaviour
 {
     private GameManager gameManager;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,5 +12,15 @@ public class gravestoneactivate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "singlepoppy")
+        {
+            Debug.Log("Holy Shit You Win!");
+        }
+
     }
 }
