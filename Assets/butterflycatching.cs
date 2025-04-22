cac8373e5fba8f7f807fa45922e38ac4b25dcc34
mounirziad <mo697861@ucf.edu>
2025-04-22 00:45:01 -0400

    netchanges

--- Assets/butterflycatching.cs
+++ Assets/butterflycatching.cs
@@ -1,16 +1,38 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class butterflycatching : MonoBehaviour
 {
     private float butterflyscaught = 0;
     public GameObject butterflyprefab;
     private PlayerManager playerManager;
+    private string originalSceneName; // Store the scene where the net belongs
 
     void Start()
     {
+        originalSceneName = SceneManager.GetActiveScene().name;
+
+
         // Find the PlayerManager in the scene
         playerManager = FindObjectOfType<PlayerManager>();
+
+        // Start checking if we're still in the correct scene
+        StartCoroutine(CheckSceneCoroutine());
+    }
+
+    private IEnumerator CheckSceneCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f); // Check every 0.5 seconds
+
+            // If we're no longer in the original scene, despawn the net
+            if (SceneManager.GetActiveScene().name != originalSceneName)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -37,7 +59,6 @@ public class butterflycatching : MonoBehaviour
         yield return new WaitForSeconds(1f);
         Debug.Log("Butterfly caught! Total: " + butterflyscaught);
 
-        // Optional: Respawn butterfly if desired
-        // Instantiate(butterflyprefab, randomPosition, Quaternion.identity);
+       
     }
 }
